ba1b71e8381d4ac5b71340c14de6016bfb7e0260
Shrena Patel <shrena.p@hotmail.co.uk>
2020-11-19 17:50:28 +1300

    styling changes

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -26,20 +26,20 @@ const Homepage = () => {
           <div className="hero">
             <div className="left-hero">
                 <div className="social-links">
-                    <p className="social-icon">G</p>
-                    <p className="social-icon">L</p>
+                    <p className="social-icon">Github</p>
+                    <p className="social-icon">LinkedIn</p>
                 </div>
             </div>
 
             <div className="right-hero">
                 <div className="intro">
-                    <p className="intro-text">Hello,</p>
+                    <p className="intro-text">Hello</p>
                     <p className="intro-text">I'm Shrena.</p>
                     <p className="intro-text">A web developer based in Wellington.</p>
                 </div>
 
                 <div className="hero-link">
-                    <p className="portfolio-link">View portfolio</p>
+                    <p className="portfolio-link">View portfolio </p>
                 </div>
             </div>
         </div>
