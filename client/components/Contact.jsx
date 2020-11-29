6d76cbb863d51ef7b50580734bc1958861d87a17
Shrena Patel <shrena.p@hotmail.co.uk>
2020-11-29 17:47:38 +1300

    simple contact page done

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -7,7 +7,12 @@ const Contact = () => {
         <div className="hero">
                 
                 <div className="hero-text">
-                    <p>Contact page</p>
+                    <p>Feel free to contact me via the links below.</p>
+                    <div className="social-links">
+                        <a href="https://github.com/shrena-patel" target="_blank" className="icon"><i className="fa fa-github fa-3x social-icon"></i></a>
+                        <a href="https://www.linkedin.com/in/shrenapatel/" target="_blank" className="icon"><i className="fa fa-linkedin fa-3x social-icon"></i></a>
+                        <a href="#" target="_blank" className="icon"><i className="fa fa-envelope fa-3x social-icon"></i></a>
+                    </div>
                     
                 </div>
 
--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Portfolio from './Portfolio'
 
-
 class Homepage extends React.Component {
     
 render () {
