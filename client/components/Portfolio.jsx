c6f875c197a6a06915527b8f151a9ea78bf53534
Shrena Patel <shrena.p@hotmail.co.uk>
2020-12-01 09:46:09 +1300

    portfolio links added

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -11,28 +11,31 @@ const Portfolio = () => {
 							<div className="content">
 								<p>KererūGo</p>
 								<p className="card-text">Final Group project at EDA.</p>
-								<button className="card-button">view code</button>
+								<a href="https://github.com/roa-2020/KereruGo" target="_blank"><button className="card-button">view code</button></a>
+								
 							</div>
 						</div>
 						<div className="portfolio-card">
 							<div className="content">
 								<p>TodoList</p>
 								<p className="card-text">A full stack todo list app.</p>
-								<button className="card-button">view code</button>
+								<a href="https://github.com/shrena-patel/todo-full-stack" target="_blank"><button className="card-button">view code</button></a>
 							</div>
 						</div>
 						<div className="portfolio-card">
 							<div className="content">
 								<p>ConnectR</p>
 								<p className="card-text">A personal project.</p>
-								<button className="card-button">view code</button>
+                                <a href="https://github.com/shrena-patel/ConnectR" target="_blank"><button className="card-button">view code</button></a>
+
 							</div>
 						</div>
 						<div className="portfolio-card">
 							<div className="content">
 								<p>Blog</p>
 								<p className="card-text">First project at EDA.</p>
-								<button className="card-button">view code</button>
+                                <a href="https://github.com/shrena-patel/shrena-patel.github.io" target="_blank"><button className="card-button">view code</button></a>
+
 							</div>
 						</div>
 					</div>
